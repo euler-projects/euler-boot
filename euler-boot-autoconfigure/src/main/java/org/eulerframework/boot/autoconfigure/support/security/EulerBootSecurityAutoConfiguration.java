92a174687019fadf09aff2d376d7bee803d8ea11
cFrost <cFrost@eulerproject.io>
2019-12-29 00:34:27 +0800

    mv security config to euler boot

--- euler-boot-autoconfigure/src/main/java/org/eulerframework/boot/autoconfigure/support/security/EulerBootSecurityAutoConfiguration.java
+++ euler-boot-autoconfigure/src/main/java/org/eulerframework/boot/autoconfigure/support/security/EulerBootSecurityAutoConfiguration.java
@@ -1,11 +1,9 @@
 package org.eulerframework.boot.autoconfigure.support.security;
 
-import org.eulerframework.boot.autoconfigure.property.EulerApplicationProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
