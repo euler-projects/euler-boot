dc414ab38eef125ef308fb7b49355c611ed52e8b
cFrost <cFrost@eulerproject.io>
2019-12-29 00:21:31 +0800

    euler security config

---
+++ euler-boot-autoconfigure/src/main/java/org/eulerframework/boot/autoconfigure/support/security/EulerBootSecurityAutoConfiguration.java
@@ -0,0 +1,56 @@
+package org.eulerframework.boot.autoconfigure.support.security;
+
+import org.eulerframework.boot.autoconfigure.property.EulerApplicationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Arrays;
+
+@Configuration
+@ConditionalOnClass(WebSecurity.class)
+@EnableConfigurationProperties({
+        EulerBootSecurityProperties.class
+})
+public class EulerBootSecurityAutoConfiguration {
+
+    @ConditionalOnClass(WebSecurity.class)
+    @Order(SecurityProperties.IGNORED_ORDER)
+    //@Configuration
+    public static class IgnoredPathsWebSecurityConfigurer
+            implements WebSecurityConfigurer<WebSecurity> {
+        private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        @Autowired
+        private EulerBootSecurityProperties eulerBootSecurityProperties;
+
+        @Override
+        public void init(WebSecurity builder) {
+            String[] ignoredPatterns = this.eulerBootSecurityProperties.getIgnoredPatterns();
+            if(ignoredPatterns != null && ignoredPatterns.length > 0) {
+                if(this.logger.isInfoEnabled()) {
+                    this.logger.info("config ignored patterns: {}", Arrays.toString(ignoredPatterns));
+                }
+
+                builder.ignoring().requestMatchers(
+                        Arrays.stream(ignoredPatterns)
+                                .map(AntPathRequestMatcher::new)
+                                .toArray(AntPathRequestMatcher[]::new));
+            }
+        }
+
+        @Override
+        public void configure(WebSecurity builder) {
+        }
+
+    }
+}
--- euler-boot-autoconfigure/src/main/java/org/eulerframework/boot/autoconfigure/support/security/EulerBootSecurityProperties.java
+++ euler-boot-autoconfigure/src/main/java/org/eulerframework/boot/autoconfigure/support/security/EulerBootSecurityProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2019 the original author or authors.
+ * Copyright 2013-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,15 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.eulerframework.boot.autoconfigure;
+package org.eulerframework.boot.autoconfigure.support.security;
 
-import org.eulerframework.boot.autoconfigure.property.EulerApplicationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Configuration
-@EnableConfigurationProperties({
-        EulerApplicationProperties.class
-})
-public class EulerBootAutoConfiguration {
+import java.util.Set;
+
+@ConfigurationProperties(prefix = "euler.security")
+public class EulerBootSecurityProperties {
+    private String[] ignoredPatterns;
+
+    public String[] getIgnoredPatterns() {
+        return ignoredPatterns;
+    }
+
+    public void setIgnoredPatterns(String[] ignoredPatterns) {
+        this.ignoredPatterns = ignoredPatterns;
+    }
 }
